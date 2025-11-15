1afae1d055d4c23ec9da4b2a13a104a9636849c4
Ion Schimbator <ionschimbator@gmail.com>
2025-11-15 10:00:34 +0200

    Add ex3

---
+++ src/MetodaAfisareCuParametru.java
@@ -0,0 +1,15 @@
+public class MetodaAfisareCuParametru {
+
+    public static void anuntDecolare(String destinatie) {
+
+        System.out.println("Zborul spre " + destinatie + " este gata de decolare!");
+
+    }
+
+    public static void main(String[] args) {
+
+        anuntDecolare("Londra");
+
+    }
+
+}
