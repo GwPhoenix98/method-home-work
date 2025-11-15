7ed05dc171331d29bc17a25f698cf871c37fcb21
Ion Schimbator <ionschimbator@gmail.com>
2025-11-15 09:43:41 +0200

    Add ex1

---
+++ src/MetodaAfisare.java
@@ -0,0 +1,16 @@
+public class MetodaAfisare {
+    public static void main(String[] args) {
+
+        MetodaAfisare afisare = new MetodaAfisare();
+
+        afisare.Mesaj();
+
+    }
+
+    void Mesaj() {
+
+        System.out.println("Hello world!");
+
+    }
+
+}
