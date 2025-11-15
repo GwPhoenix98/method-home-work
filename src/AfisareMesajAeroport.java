1271726cc2b8a9273d2c1777e271153a64f11722
Ion Schimbator <ionschimbator@gmail.com>
2025-11-15 09:50:41 +0200

    Add ex2

---
+++ src/AfisareMesajAeroport.java
@@ -0,0 +1,9 @@
+public class AfisareMesajAeroport {
+    public static void main(String[] args) {
+
+        MesajAeroport afis = new MesajAeroport();
+
+        afis.mesajBunVenit();
+
+    }
+}
---
+++ src/MesajAeroport.java
@@ -0,0 +1,8 @@
+public class MesajAeroport {
+
+    void mesajBunVenit() {
+
+        System.out.println("Bun venit in aeroportul din Londra!");
+
+    }
+}
